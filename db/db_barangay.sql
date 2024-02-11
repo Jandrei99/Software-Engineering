c065bbea6b38b006e8dc08b57585c8990ae67c38
Joshua Carreon <126605596+Jandrei99@users.noreply.github.com>
2024-02-11 18:56:29 +0800

    Add files via upload

---
+++ db/db_barangay.sql
@@ -0,0 +1,481 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 24, 2024 at 10:23 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_barangay`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblactivity`
+--
+
+CREATE TABLE `tblactivity` (
+  `id` int(11) NOT NULL,
+  `dateofactivity` date NOT NULL,
+  `activity` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblactivityphoto`
+--
+
+CREATE TABLE `tblactivityphoto` (
+  `id` int(11) NOT NULL,
+  `activityid` int(11) NOT NULL,
+  `filename` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblactivityphoto`
+--
+
+INSERT INTO `tblactivityphoto` (`id`, `activityid`, `filename`) VALUES
+(18, 7, '1485255503893ChibiMaker.jpg'),
+(19, 7, '1485255504014dental.jpg'),
+(20, 7, '1485255504108images.jpg'),
+(21, 8, '1485255608251dfxfxfxdfxfxfxdf.png'),
+(22, 8, '1485255608315easy-nail-art-designs-for-beginners-youtube.jpg'),
+(23, 8, '1485255608404Easy-Winter-Nail-Art-Tutorials-2013-2014-For-Beginners-Learners-10.jpg'),
+(24, 8, '1485255608513motherboard.png'),
+(25, 9, '148525575293111041019_1012143402147589_9043399646875097729_n.jpg'),
+(26, 9, '1485255753089bg.PNG'),
+(32, 10, '148526764905211041019_1012143402147589_9043399646875097729_n.jpg'),
+(33, 10, '1485267649364bg.PNG'),
+(34, 10, '1485267649563motherboard.png'),
+(35, 10, '14855301764078196186971_2237f161bd_b.jpg'),
+(36, 10, '1485530481111bicycle-1280x720.jpg'),
+(38, 11, '1485530620716user2.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblblotter`
+--
+
+CREATE TABLE `tblblotter` (
+  `id` int(11) NOT NULL,
+  `yearRecorded` varchar(4) NOT NULL,
+  `dateRecorded` date NOT NULL,
+  `complainant` text NOT NULL,
+  `cage` int(11) NOT NULL,
+  `caddress` text NOT NULL,
+  `ccontact` int(11) NOT NULL,
+  `personToComplain` text NOT NULL,
+  `page` int(11) NOT NULL,
+  `paddress` text NOT NULL,
+  `pcontact` int(11) NOT NULL,
+  `complaint` text NOT NULL,
+  `actionTaken` varchar(50) NOT NULL,
+  `sStatus` varchar(50) NOT NULL,
+  `locationOfIncidence` text NOT NULL,
+  `recordedby` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblblotter`
+--
+
+INSERT INTO `tblblotter` (`id`, `yearRecorded`, `dateRecorded`, `complainant`, `cage`, `caddress`, `ccontact`, `personToComplain`, `page`, `paddress`, `pcontact`, `complaint`, `actionTaken`, `sStatus`, `locationOfIncidence`, `recordedby`) VALUES
+(5, '2024', '2024-01-22', 'Deorayom, John Joshua Salandanan', 0, '', 0, '', 0, '', 0, 'wewqe', '1st Option', 'Solved', '', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblclearance`
+--
+
+CREATE TABLE `tblclearance` (
+  `id` int(11) NOT NULL,
+  `clearanceNo` int(11) NOT NULL,
+  `residentid` int(11) NOT NULL,
+  `findings` text NOT NULL,
+  `purpose` text NOT NULL,
+  `orNo` int(11) NOT NULL,
+  `samount` int(11) NOT NULL,
+  `dateRecorded` date NOT NULL,
+  `recordedBy` varchar(50) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblclearance`
+--
+
+INSERT INTO `tblclearance` (`id`, `clearanceNo`, `residentid`, `findings`, `purpose`, `orNo`, `samount`, `dateRecorded`, `recordedBy`, `status`) VALUES
+(8, 0, 11, '', 'asd', 0, 0, '2017-01-20', 'User1', 'New'),
+(13, 1, 17, '', 'EMPLOYMENT', 1, 30, '2024-01-22', 'John Joshua', 'Approved');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblhousehold`
+--
+
+CREATE TABLE `tblhousehold` (
+  `id` int(11) NOT NULL,
+  `householdno` int(11) NOT NULL,
+  `zone` varchar(11) NOT NULL,
+  `totalhouseholdmembers` int(2) NOT NULL,
+  `headoffamily` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbllogs`
+--
+
+CREATE TABLE `tbllogs` (
+  `id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `logdate` datetime NOT NULL,
+  `action` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbllogs`
+--
+
+INSERT INTO `tbllogs` (`id`, `user`, `logdate`, `action`) VALUES
+(43, 'Administrator', '2024-01-22 12:30:15', 'Added Blotter Request by Deorayom, John Joshua Salandanan'),
+(44, 'Administrator', '2024-01-22 12:30:44', 'Update Blotter Request by Deorayom, John Joshua Salandanan'),
+(45, 'Administrator', '2024-01-22 12:32:04', 'Added Clearance with clearance number of 1'),
+(46, 'Administrator', '2024-01-22 12:40:15', 'Added Zone number 2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblofficial`
+--
+
+CREATE TABLE `tblofficial` (
+  `id` int(11) NOT NULL,
+  `sPosition` varchar(50) NOT NULL,
+  `completeName` text NOT NULL,
+  `pcontact` varchar(20) NOT NULL,
+  `paddress` text NOT NULL,
+  `termStart` date NOT NULL,
+  `termEnd` date NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblofficial`
+--
+
+INSERT INTO `tblofficial` (`id`, `sPosition`, `completeName`, `pcontact`, `paddress`, `termStart`, `termEnd`, `status`) VALUES
+(4, 'Captain', 'Salandanan, Dennis Menguito', '8978768761', 'Ilaya, Malaban', '2016-10-03', '2016-10-06', 'Ongoing Term');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpermit`
+--
+
+CREATE TABLE `tblpermit` (
+  `id` int(11) NOT NULL,
+  `residentid` int(11) NOT NULL,
+  `businessName` text NOT NULL,
+  `businessAddress` text NOT NULL,
+  `typeOfBusiness` varchar(50) NOT NULL,
+  `orNo` int(11) NOT NULL,
+  `samount` int(11) NOT NULL,
+  `dateRecorded` date NOT NULL,
+  `recordedBy` varchar(50) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblpermit`
+--
+
+INSERT INTO `tblpermit` (`id`, `residentid`, `businessName`, `businessAddress`, `typeOfBusiness`, `orNo`, `samount`, `dateRecorded`, `recordedBy`, `status`) VALUES
+(2, 11, 'test', 'test', 'Option 2', 213, 2132131, '2016-10-15', '', 'Disapproved'),
+(5, 11, 'Business ', 'Address', 'Option 1', 0, 0, '2016-12-04', 'a', 'New'),
+(7, 11, 'sad', 'asd', 'Option 2', 0, 0, '2017-01-20', 'a', 'New');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblresident`
+--
+
+CREATE TABLE `tblresident` (
+  `id` int(11) NOT NULL,
+  `lname` varchar(20) NOT NULL,
+  `fname` varchar(20) NOT NULL,
+  `mname` varchar(20) NOT NULL,
+  `bdate` varchar(20) NOT NULL,
+  `bplace` text NOT NULL,
+  `age` int(11) NOT NULL,
+  `barangay` varchar(120) NOT NULL,
+  `zone` varchar(5) NOT NULL,
+  `totalhousehold` int(5) NOT NULL,
+  `differentlyabledperson` varchar(100) NOT NULL,
+  `relationtohead` varchar(50) NOT NULL,
+  `maritalstatus` varchar(50) NOT NULL,
+  `bloodtype` varchar(10) NOT NULL,
+  `civilstatus` varchar(20) NOT NULL,
+  `occupation` varchar(100) NOT NULL,
+  `monthlyincome` int(12) NOT NULL,
+  `householdnum` int(11) NOT NULL,
+  `lengthofstay` int(11) NOT NULL,
+  `religion` varchar(50) NOT NULL,
+  `nationality` varchar(50) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `skills` text NOT NULL,
+  `igpitID` int(11) NOT NULL,
+  `philhealthNo` int(12) NOT NULL,
+  `highestEducationalAttainment` varchar(50) NOT NULL,
+  `houseOwnershipStatus` varchar(50) NOT NULL,
+  `landOwnershipStatus` varchar(20) NOT NULL,
+  `dwellingtype` varchar(20) NOT NULL,
+  `waterUsage` varchar(20) NOT NULL,
+  `lightningFacilities` varchar(20) NOT NULL,
+  `sanitaryToilet` varchar(20) NOT NULL,
+  `formerAddress` text NOT NULL,
+  `remarks` text NOT NULL,
+  `image` text NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblresident`
+--
+
+INSERT INTO `tblresident` (`id`, `lname`, `fname`, `mname`, `bdate`, `bplace`, `age`, `barangay`, `zone`, `totalhousehold`, `differentlyabledperson`, `relationtohead`, `maritalstatus`, `bloodtype`, `civilstatus`, `occupation`, `monthlyincome`, `householdnum`, `lengthofstay`, `religion`, `nationality`, `gender`, `skills`, `igpitID`, `philhealthNo`, `highestEducationalAttainment`, `houseOwnershipStatus`, `landOwnershipStatus`, `dwellingtype`, `waterUsage`, `lightningFacilities`, `sanitaryToilet`, `formerAddress`, `remarks`, `image`, `username`, `password`) VALUES
+(17, 'Deorayom', 'John Joshua', 'Salandanan', '1998-04-17', 'Binan, Laguna', 25, 'Malaban', '2', 7, '', 'Grandson', 'Single', 'O+', 'Single', 'N/A', 5000, 5141, 300, 'Catholic', 'Filipino', 'Male', 'N/A', 1, 0, 'College, undergrad', 'Own Home', 'Owned', '1st Option', 'Faucet', 'Electric', 'Laguna Water', '', '', '1705888766916_406207449_6811116982290400_4451035217416136545_n.jpg', 'joshuadeorayom', 'superowa17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstaff`
+--
+
+CREATE TABLE `tblstaff` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblstaff`
+--
+
+INSERT INTO `tblstaff` (`id`, `name`, `username`, `password`) VALUES
+(1, 'Deorayom, John Joshua Salandanan', 'owa17', 'superowa17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluser`
+--
+
+CREATE TABLE `tbluser` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbluser`
+--
+
+INSERT INTO `tbluser` (`id`, `username`, `password`, `type`) VALUES
+(1, 'admin', 'admin', 'administrator'),
+(2, 'zone', '1234', 'zoneleader');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblzone`
+--
+
+CREATE TABLE `tblzone` (
+  `id` int(5) NOT NULL,
+  `zone` varchar(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblzone`
+--
+
+INSERT INTO `tblzone` (`id`, `zone`, `username`, `password`) VALUES
+(0, '2', 'owa17', 'superowa17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblactivity`
+--
+ALTER TABLE `tblactivity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblactivityphoto`
+--
+ALTER TABLE `tblactivityphoto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblblotter`
+--
+ALTER TABLE `tblblotter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblclearance`
+--
+ALTER TABLE `tblclearance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblhousehold`
+--
+ALTER TABLE `tblhousehold`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbllogs`
+--
+ALTER TABLE `tbllogs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblofficial`
+--
+ALTER TABLE `tblofficial`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblpermit`
+--
+ALTER TABLE `tblpermit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblresident`
+--
+ALTER TABLE `tblresident`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblstaff`
+--
+ALTER TABLE `tblstaff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblzone`
+--
+ALTER TABLE `tblzone`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblactivity`
+--
+ALTER TABLE `tblactivity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tblactivityphoto`
+--
+ALTER TABLE `tblactivityphoto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `tblblotter`
+--
+ALTER TABLE `tblblotter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblclearance`
+--
+ALTER TABLE `tblclearance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tblhousehold`
+--
+ALTER TABLE `tblhousehold`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbllogs`
+--
+ALTER TABLE `tbllogs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `tblofficial`
+--
+ALTER TABLE `tblofficial`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tblpermit`
+--
+ALTER TABLE `tblpermit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tblresident`
+--
+ALTER TABLE `tblresident`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `tblstaff`
+--
+ALTER TABLE `tblstaff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
